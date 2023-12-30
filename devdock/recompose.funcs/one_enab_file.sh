b8b1fc05a728cfb2f3dff30fb2a2a090b6d635a6
mk-pmb <mk-pmb@users.noreply.github.com>
2023-12-30 22:31:33 +0100

    DevDock: Factor out function `devdock_recompose__one_enab`.

---
+++ devdock/recompose.funcs/one_enab_file.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+
+
+function devdock_recompose__one_enab () {
+  local YAML="$(cat -- "$ENAB_FILE")"
+  local NECK=$'\nservices:\n'
+  local HEAD="${YAML%%$NECK*}"
+  [ "$HEAD" != "$YAML" ] || return 4$(
+    echo "E: Cannot find 'services:' line in $ENAB_FILE" >&2)
+  YAML="${YAML:${#HEAD}}"
+  YAML="${YAML:${#NECK}}"
+
+  HEAD="$(<<<"$HEAD" "$DD_PROGDIR"/src/denoise_yaml_header.sed)"
+
+  case "$HEAD" in
+    'version:3' ) ;;
+
+    '' )
+      echo "E: Template $ENAB_FILE" \
+        "must declare at least the docker-compose version used," \
+        "which must be exactly $DOCO_VER." >&2
+      return 7;;
+
+    * )
+      echo "E: Unsupported header line(s) in $ENAB_FILE:" \
+        "'${HEAD//$'\n'/¶ }'" >&2
+      return 7;;
+  esac
+
+  YAML="${YAML%$'\n'}"
+  YAML="${YAML%$'\n...'}"
+  if [[ "$YAML"$'\n' == *$'\n...\n'* ]]; then
+    echo "E: Unexpected '...' line in $ENAB_FILE" >&2
+    return 4
+  fi
+  if [[ "$YAML" == *'${#env_secret}'* ]]; then
+    echo "E: Your project uses the deprecated ENV_SECRETS feature." >&2
+    return 4
+  fi
+
+  YAML="${YAML#$'\n'}"
+  YAML="${YAML%$'\n'}"
+  YAML="$(<<<"$YAML" "$DD_PROGDIR"/src/highlight_slots.sed)"
+
+  local ENAB_BFN="$(basename -- "$ENAB_FILE")"
+  YAML="${YAML//$'\f<var dd_dir >'/$DD_DIR/}"
+  YAML="${YAML//$'\f<var dd_proj >'/$DD_PROJ}"
+  YAML="${YAML//$'\f<var dd_tpl >'/$ENAB_BFN}"
+  YAML="${YAML//$'\f<var dd_tpl_bn >'/${ENAB_BFN%.yaml}}"
+
+  local KEY= VAL=
+  for KEY in "${!CFG[@]}"; do
+    VAL="${CFG[$KEY]}"
+    YAML="${YAML//$'\f<dd_cfg '"$KEY >"/$VAL}"
+  done
+
+  local MISS=()
+  readarray -t MISS < <(grep -oPe '\f<[^<>]+>' <<<"$YAML" \
+    | cut --bytes=2- | LANG=C sort --unique)
+  [ "${#MISS[@]}" == 0 ] || return 4$(
+    echo "E: Unsolved slots in $ENAB_FILE: ${MISS[*]}" >&2)
+
+  C_GEN+=$'\n\n'
+  C_GEN+="# >>> services from $ENAB_FILE >>>"$'\n'
+  C_GEN+="$YAML"$'\n'
+  C_GEN+="# <<< services from $ENAB_FILE <<<"$'\n'
+}
+
+
+return 0

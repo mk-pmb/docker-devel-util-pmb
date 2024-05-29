mk-pmb/docker-devel-util-pmb

commit 1fbd1323fbe2a038da45a10d3c3692462ad38e28

docompose: Wrap the actual docker action in a separate function.

dockerized-docker-compose.sh
@@ -64,6 +64,13 @@ function dockerized_docker_compose () {
     docker/compose:latest
     )
 
+  doco_fallible_actually_do_stuff; local D_RV=$?
+
+  return "$D_RV"
+}
+
+
+function doco_fallible_actually_do_stuff () {
   local D_CMD=(
     "${OUTER_RUN[@]}"
     "$D_TASK"
@@ -71,9 +78,7 @@ function dockerized_docker_compose () {
     "$@"
     )
   [ "$DBGLV" -lt 2 ] || echo "D: ${D_CMD[*]}" >&2
-  "${D_CMD[@]}"; local D_RV=$?
-
-  return "$D_RV"
+  "${D_CMD[@]}" || return $?
 }
 
 

commit 24907d58568a3af4fabc552c8d0499f6ca424d18

docompose: Pass CLI args to fallible core.
Bug was introduced in commit 1fbd1323fbe2a038da45a10d3c3692462ad38e28.

dockerized-docker-compose.sh
@@ -87,7 +87,7 @@ function dockerized_docker_compose () {
     docker/compose:latest
     )
 
-  doco_fallible_actually_do_stuff; local D_RV=$?
+  doco_fallible_actually_do_stuff "$@"; local D_RV=$?
 
   [ -z "$STERN_WARNINGS" ] || echo "W: $APP_NAME:" \
     "In case there was a lot of output above," \
ad4ff34f216b53b00fdbe08fbf111e5bb33be5f6
mk-pmb <mk-pmb@users.noreply.github.com>
2024-12-01 22:20:58 +0100

    DevDock: Support disabling files via symlink to /dev/null.

--- devdock/core.funcs/find_subdir_files.sh
+++ devdock/core.funcs/find_subdir_files.sh
@@ -3,10 +3,29 @@
 
 
 function devdock_find_subdir_files () {
-  local ITEM= FEXT="$1"; shift
-  for ITEM in "$@"; do
-    for ITEM in "$ITEM"{,.site,.local,.@"$HOSTNAME"}/*."$FEXT"; do
-      [ ! -f "$ITEM" ] || echo "$ITEM"
+  local FEXT="$1"; shift
+  local SUFFIXES=( # generic s. go first, most specific s. goes last
+    ''
+    .site
+    .local
+    .@"$HOSTNAME"
+    )
+  local CATEG= ITEM= SUBDIR=
+  local FOUND=() IGNORE=
+  for CATEG in "$@"; do
+    FOUND=()
+    IGNORE=$'\n'
+    for ITEM in "${SUFFIXES[@]}"; do
+      for ITEM in "$CATEG$ITEM"/*."$FEXT"; do
+        if [ -L "$ITEM" ] && [ "$ITEM" -ef /dev/null ]; then
+          IGNORE+="$(basename -- "$ITEM")"$'\n'
+        elif [ -f "$ITEM" ]; then
+          FOUND+=( "$ITEM" )
+        fi
+      done
+    done
+    for ITEM in "${FOUND[@]}"; do
+      [[ "$IGNORE" == *$'\n'"$(basename -- "$ITEM")"$'\n'* ]] || echo "$ITEM"
     done
   done
 }
